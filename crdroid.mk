461ab872ac41e32c4d7ee5ec661d79a676fd43aa
Monty Kumar <aryanmk.mk5@gmail.com>
2017-05-30 05:17:43 -0700

    Update and rename lineage.mk to crdroid.mk

--- crdroid.mk
+++ crdroid.mk
@@ -4,14 +4,14 @@
 PRODUCT_RELEASE_NAME := hm_note_1w
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Xiaomi/hm_note_1w/device_hm_note_1w.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hm_note_1w
-PRODUCT_NAME := cm_hm_note_1w
+PRODUCT_NAME := crdroid_hm_note_1w
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 3G
 PRODUCT_MANUFACTURER := Xiaomi
