edb34fe17acbc8f8f3ade873bcc5e960d27328ab
Monty Kumar <aryanmk.mk5@gmail.com>
2017-05-30 05:40:32 -0700

    Update crdroid.mk

--- crdroid.mk
+++ crdroid.mk
@@ -7,7 +7,7 @@ PRODUCT_RELEASE_NAME := hm_note_1w
 $(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Xiaomi/hm_note_1w/device_hm_note_1w.mk)
+$(call inherit-product, device/Xiaomi/hm_note_1w/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hm_note_1w
